ef3349738c4956beb914a0d87c222ce2ce714632
pratik111111 <pratikkhotekar2024@gmail.com>
2025-02-14 11:17:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-//const messages = [
-  //  "Are you sure?",
-   // "Really sure??",
- //   "Are you positive?",
-  //  "Pookie please...",
-  //  "Just think about it!",
-  //  "If you say no, I will be really sad...",
-  //  "I will be very sad...",
-   // "I will be very very very sad...",
-   // "Ok fine, I will stop asking...",
-   // "Just kidding, say yes please! ❤️"
-//];
 const messages = [
     "Kya tum pakka ho? 🤔",
     "Sach mein sure ho?? 😳",
